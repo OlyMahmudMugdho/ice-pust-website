41f89a1b95be6970768fc5565dba1469182bdac8
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-01-05 13:51:17 +0600

    Update syllabus-script.js

--- ice-pust/academic/syllabus-script.js
+++ ice-pust/academic/syllabus-script.js
@@ -1,5 +1,24 @@
-$(document).ready(function () {
-    $(".not-hidden").click(function () {
-        $(".u li:not(.dropdown-content)").toggleClass("links");
-    });
-});
+let links = document.querySelectorAll(".u > .links");
+let icon = document.getElementsByClassName("fa")[0].classList;
+let is_hidden = true;
+
+const toggle = () => {
+    if (is_hidden == true) {
+        icon.remove("fa-bars");
+        icon.add("fa-minus");
+        for (let index = 0; index < links.length; index++) {
+            links[index].style.display = "initial";
+        }
+        is_hidden = false;
+        return is_hidden;
+    }
+    else {
+        icon.remove("fa-minus");
+        icon.add("fa-bars");
+        for (let index = 0; index < links.length; index++) {
+            links[index].style.display = "none";
+        }
+        is_hidden = true;
+        return is_hidden;
+    }
+}
